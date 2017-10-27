fc70c5127b26bab14d38181fba91642518710e05
gstamac <gstamac>
2017-10-27 10:43:17 +0200

    changed JSON files list format (use output to provide output filename)

--- convert-from-ts.js
+++ convert-from-ts.js
@@ -7,18 +7,19 @@ var config = JSON.parse(fs.readFileSync('ts2fable-config.json').toString())
 
 var convertedFiles = []
 
-function addWithImports(filePath, imports, visited) {
+function addWithImports(file, imports, visited) {
+    var filePath = file.filename;
     if ((visited.indexOf(filePath) >= 0)) return;
     visited.push(filePath);
-    if ((imports.indexOf(filePath) >= 0)) return;
+    if (imports.find(f => f.filename == filePath)) return;
     var code = fs.readFileSync(filePath).toString().replace(/readonly/g, "");
     var sourceFile = ts.createSourceFile(filePath, code, ts.ScriptTarget.ES6, /*setParentNodes*/ true);
     var fileDir = path.posix.dirname(filePath);
     ts.forEachChild(sourceFile, node => {
         if (!node.importClause || !node.moduleSpecifier) return;
         var importFilename = path.posix.join(fileDir, node.moduleSpecifier.text) + ".d.ts";
-        if ((imports.indexOf(importFilename) < 0) && fs.existsSync(importFilename)) {
-            addWithImports(importFilename, imports, visited);
+        if (!imports.find(f => f.filename == importFilename) && fs.existsSync(importFilename)) {
+            addWithImports({ filename: importFilename }, imports, visited);
         } else {
             importFilename = path.posix.join(fileDir, node.moduleSpecifier.text, "index.d.ts");
             var importFileDir = path.posix.dirname(importFilename);
@@ -32,20 +33,20 @@ function addWithImports(filePath, imports, visited) {
                 if (importElements && importElements.length == 0) return;
                 if (!node1.exportClause || !node1.moduleSpecifier || (node1.moduleSpecifier.text.indexOf('index') >= 0)) return
                 var importFilename2 = path.posix.join(importFileDir, node1.moduleSpecifier.text) + ".d.ts";
-                if ((imports.indexOf(importFilename2) >= 0) || !fs.existsSync(importFilename2)) return;
+                if (imports.find(f => f.filename == importFilename2) || !fs.existsSync(importFilename2)) return;
                 var exportElements = node1.exportClause.elements
                     .filter(e => e.name && e.name.originalKeywordKind != ts.SyntaxKind.DefaultKeyword)
                     .map(e => e.name.escapedText)
                 var exportExists = importElements == undefined 
                     || exportElements.find(e => importElements.indexOf(e) >= 0) !== undefined;
                 if (!exportExists) return;
-                addWithImports(importFilename2, imports, visited);
+                addWithImports({ filename: importFilename2 }, imports, visited);
                 if (importElements) 
                     importElements = importElements.filter(e => exportElements.indexOf(e) < 0);
             });
         }                
     });
-    imports.push(filePath);
+    imports.push(file);
 }
 
 function convert(tsfile, fablefile) 
@@ -79,15 +80,17 @@ function deleteDir(dirname)
 }
 
 shell.exec("cat /dev/null > type-cache.json")
+shell.mkdir('-p', 'converted');
+
 
 var tsFiles = config.files.map(f => {
     if (f.filename)
-        return f.filename;
+        return f;
     else if (f.include) 
-        return shell.ls(f.include).filter(fn => !f.exclude || !(new RegExp(f.exclude).exec(fn)));
+        return shell.ls(f.include).filter(fn => !f.exclude || !(new RegExp(f.exclude).exec(fn))).map(fn => { return { filename: fn }});
     else    
         return undefined;
-}).filter(f => f).reduce((flat, next) => flat.concat(next), []);
+}).reduce((flat, next) => flat.concat(next), []).filter(f => f);
 
 var files = []
 if (config.skipImports)
@@ -97,11 +100,11 @@ else {
     tsFiles.forEach(fn => addWithImports(fn, files, visited));
 }
 
-files.forEach(fn => convert(fn));
+files.forEach(fn => convert(fn.filename, fn.output));
 
 var projSource = fs.readFileSync(config.fsprojName + ".fsproj-TEMPLATE").toString()
     .replace("[FS_FILES]", convertedFiles.map(fn => '    <Compile Include="' + fn + '" />').join("\n"));
 fs.writeFileSync(config.fsprojName + ".fsproj", projSource);
 
 shell.ls('converted/**/*.fs').filter(f => convertedFiles.indexOf(f) == -1).forEach(deleteFile);
-shell.find('converted').filter(fn => shell.test('-d', fn) && (shell.ls(fn).length == 0)).forEach(deleteDir);
+shell.find('converted').filter(fn => fn != 'converted' && shell.test('-d', fn) && (shell.ls(fn).length == 0)).forEach(deleteDir);
